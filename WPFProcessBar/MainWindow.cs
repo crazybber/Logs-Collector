add13d8db3b9eceb81a845cd934ff8ed4877b85b
Eamon <Chenlulei@live.com>
2015-09-22 11:19:12 +0800

    refactoring ProcessBar Demo

--- WPFProcessBar/MainWindow.cs
+++ WPFProcessBar/MainWindow.cs
@@ -1,25 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace WPFProcessBar
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
@@ -31,13 +18,10 @@ namespace WPFProcessBar
         private void Grid_Loaded(object sender, RoutedEventArgs e)
         {
             Thread.Sleep(1000);
-
-
         }
 
         private void Button1_Click(object sender, RoutedEventArgs e)
         {
-
             updateprocess4();
             //updateprocess3();
             //updateprocess2();
@@ -52,11 +36,9 @@ namespace WPFProcessBar
             //{
             //    ProgressBar1.Value += 20;
             //}
-
-
         }
 
-        void updateprocess4()
+        private void updateprocess4()
         {
             ProgressBar1.Minimum = 0;
 
@@ -64,32 +46,25 @@ namespace WPFProcessBar
 
             ProgressBar1.Value = 0;
 
-
-            var averageStep = short.MaxValue / ProgressBar1.ActualWidth;
+            var averageStep = short.MaxValue/ProgressBar1.ActualWidth;
 
             var n = 0;
 
             while (ProgressBar1.Value < ProgressBar1.Maximum)
             {
-
                 ProgressBar1.Value += 1;
 
                 n += 1;
 
                 if (!(n >= averageStep)) continue;
-                Dispatcher.Invoke(() =>
-                {
-
-
-
-                }, DispatcherPriority.Background);
+                Dispatcher.Invoke(() => { }, DispatcherPriority.Background);
 
                 n = 0;
             }
         }
-        void updateprocess3()
-        {
 
+        private void updateprocess3()
+        {
             ProgressBar1.Minimum = 0;
 
             ProgressBar1.Maximum = short.MaxValue;
@@ -99,19 +74,12 @@ namespace WPFProcessBar
             while (ProgressBar1.Value < ProgressBar1.Maximum)
             {
                 ProgressBar1.Value += 1;
-                Dispatcher.Invoke(() =>
-                {
-
-
-
-                }, DispatcherPriority.Background);
-
+                Dispatcher.Invoke(() => { }, DispatcherPriority.Background);
             }
         }
 
-        void updateprocess2()
+        private void updateprocess2()
         {
-
             ProgressBar1.Minimum = 0;
 
             ProgressBar1.Maximum = short.MaxValue;
@@ -120,18 +88,11 @@ namespace WPFProcessBar
 
             while (ProgressBar1.Value < ProgressBar1.Maximum)
             {
-
-                Dispatcher.Invoke(() =>
-                {
-
-                    ProgressBar1.Value += 1;
-
-                }, DispatcherPriority.Background);
-
+                Dispatcher.Invoke(() => { ProgressBar1.Value += 1; }, DispatcherPriority.Background);
             }
         }
 
-        void updateprocess1()
+        private void updateprocess1()
         {
             ProgressBar1.Maximum = short.MaxValue; //模拟一个耗时计算
 
@@ -139,21 +100,15 @@ namespace WPFProcessBar
 
             while (ProgressBar1.Value < ProgressBar1.Maximum)
             {
-
                 ProgressBar1.Value += 1;
                 Thread.Sleep(10);
-
             }
         }
 
-        void updateprocess()
+        private void updateprocess()
         {
-
-
-
             ProgressBar1.Dispatcher.Invoke(DispatcherPriority.Normal, new Action(() =>
             {
-
                 for (var i = 0; i < 100; i++)
                 {
                     ProgressBar1.Value += 1;
@@ -162,9 +117,7 @@ namespace WPFProcessBar
 
                 if (ProgressBar1.Value < 100)
                 {
-
                 }
-
             }));
         }
     }
